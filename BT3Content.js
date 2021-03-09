1c4e3e265a89a238a03f353d4441bbe0cabd367c
NguyenXuanCanh <x.canh040701@gmail.com>
2021-03-09 18:37:21 +0700

    upload

---
+++ BT3Content.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import BT3Carousel from './BT3Carousel'
+import BT3ProductList from './BT3ProductList'
+
+export default class BT3Content extends Component {
+    render() {
+        return (
+            <div className="container pt-5">
+                <BT3Carousel></BT3Carousel>
+                <BT3ProductList></BT3ProductList>
+            </div>
+        )
+    }
+}
